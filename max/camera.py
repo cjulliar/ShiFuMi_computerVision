ce67b705622cd286b4effeeb8b2512513193c643
Maxime KUIL <maximekuil@gmail.com>
2024-06-28 16:55:24 +0200

    test camera

--- max/camera.py
+++ max/camera.py
@@ -1,46 +1,53 @@
 # Import the InferencePipeline object
+from functools import partial
+import json
+import os
 from typing import Any, List
 from inference import InferencePipeline
 
 # Import the built in render_boxes sink for visualizing results
 from inference.core.interfaces.stream.sinks import render_boxes
 from inference.core.interfaces.camera.entities import VideoFrame
-import torch
+from inference.core.interfaces.stream.sinks import UDPSink
+
 from ultralytics import YOLO
+
+TARGET_DIR = "./my_predictions"
 
 
 class MyModel:
     def __init__(self, weights_path: str):
-        # self._model = torch.hub.load(
-        #     "ultralytics/yolov9", "custom", path=weights_path, force_reload=True
-        # )
+        self._model = YOLO(weights_path, task="detect")
 
-        self._model = YOLO(
-            "/teamspace/studios/this_studio/runs/detect/train7/weights/best.torchscript" 
-        )
-
-        # with torch.inference_mode():
-        
-
-        self._model(
-            "/teamspace/studios/this_studio/Rock-Paper-Scissors-SXSW-14/test/images/egohands-public-1621349890675_png_jpg.rf.0d6f828c1a48a7515a87a85c468da676.jpg",
-        )
-
-    # after v0.9.18
     def infer(self, video_frames: List[VideoFrame]) -> List[Any]:
-        # result must be returned as list of elements representing model prediction for single frame
-        # with order unchanged.
-        with torch.inference_mode():
-            return self._model([v.image for v in video_frames])
+        return self._model([v.image for v in video_frames])
+
+    def on_prediction(predictions, video_frame) -> None:
+        with open(os.path.join(TARGET_DIR, f"{video_frame.frame_id}.json")) as f:
+            json.dump(predictions, f)
 
 
 my_model = MyModel(
-    "/Users/maximekuil/Documents/Simplon/ShiFuMi_computerVision/shifumi_trained.pt"
+    "/Users/maximekuil/Documents/Simplon/ShiFuMi_computerVision/models/shifumi_trained_yolo9t.torchscript"
 )
-# pipeline = InferencePipeline.init_with_custom_logic(
-#     on_video_frame=my_model.infer,
-#     video_reference=1,  # Path to video, device id (int, usually 0 for built in webcams), or RTSP stream url
-#     on_prediction=render_boxes,  # Function to run after each prediction
-# )
-# pipeline.start()
-# pipeline.join()
+
+
+def on_prediction(predictions, video_frame):
+    render_boxes(
+        predictions=predictions,
+        video_frame=video_frame,
+        display_statistics=True,  # Enable displaying statistics
+    )
+
+
+udp_sink = UDPSink.init(ip_address="127.0.0.1", port=9090)
+
+pipeline = InferencePipeline.init_with_custom_logic(
+    on_video_frame=my_model.infer,
+    video_reference=1,
+    # on_prediction=my_model.on_prediction,
+    # on_prediction=on_prediction,  # Function to run after each prediction
+)
+
+pipeline.start()
+pipeline.join()
